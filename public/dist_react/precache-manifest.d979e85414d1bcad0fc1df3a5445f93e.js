5ccfeca7dec572e5c478826a6e9088ecd5d29b0b
pradipsolace <pradip.kushare@solacetechnologies.co.in>
2020-12-22 13:08:29 +0530

    first com

---
+++ public/dist_react/precache-manifest.d979e85414d1bcad0fc1df3a5445f93e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d59b5643b9cae2347c2f42dc1dc38fea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e150fb68b1d16bc2d5a7",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "13a86bccad9b44d877ca",
+    "url": "/static/css/main.ffe338ff.chunk.css"
+  },
+  {
+    "revision": "e150fb68b1d16bc2d5a7",
+    "url": "/static/js/2.18ebf7fd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.18ebf7fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13a86bccad9b44d877ca",
+    "url": "/static/js/main.9ad2671c.chunk.js"
+  },
+  {
+    "revision": "9a3c03ee52fa7ca08c28",
+    "url": "/static/js/runtime-main.a5d49e5c.js"
+  }
+]);
